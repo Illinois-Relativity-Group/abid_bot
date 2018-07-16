5b7ea71a73eb8445071bd35ecf589af3e0a35556
Kyle Christopher Nelli <nelli@h2ologin1.ncsa.illinois.edu>
2018-07-16 12:02:09 -0500

    Links bh data too

--- abid_bot_v2.7/bin/link_h5data.sh
+++ abid_bot_v2.7/bin/link_h5data.sh
@@ -5,12 +5,20 @@ fi
 
 #full path to actual data
 origin="/u/sciteam/ruiz1/scratch/Antonios/Spin_NSNS/med/Magnetized/spHd2.5_K123.6_131_mcor/beta100/"
-#full path to h5dir
-dest=$root/h5data/ 
+#full path to h5dir and bh data
+h5dest=$root/h5data/
+bhdest=$root/horizon/all_horizon/
 
 for i in $(ls -d $origin/3d* | xargs -n 1 basename); do #just file name no path
-	if [ ! -d $dest/$i ]; then 
+	if [ ! -d $h5dest/$i ]; then 
 		echo Linking $i
-		ln -s $origin/$i $dest
+		ln -s $origin/$i $h5dest
 	fi
+done
+
+for i in $(ls $origin/h.t*.gp | xargs -n 1 basename); do
+    if [ ! -f $bhdest/$i ]; then
+        echo Linking $i
+        ln -s $origin/$i $bhdest/
+    fi
 done
