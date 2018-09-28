846728197d67f2267fb82144c0c950982357a17b
Kyle Nelli <kcnelli21@gmail.com>
2018-09-28 13:37:04 -0500

    Don't delete data

--- abid_bot_v2.7/bin/bw_many_folder_scripts/rmdupes.py
+++ abid_bot_v2.7/bin/bw_many_folder_scripts/rmdupes.py
@@ -1,4 +1,4 @@
-from shutil import rmtree
+from shutil import move,rmtree
 from os import unlink
 from sys import argv
 
@@ -7,8 +7,10 @@ f = open(root + "bin/bw_many_folder_scripts/duplicate.txt", 'r')
 f.readline()
 f.readline()
 for line in f:
-	try: rmtree(line[:-1])		#for actual files
-	except: unlink(line[:-1])	#for symlinks
+	move(line[:-1],line[:-22] + "bad_data/" + line[-22:-1])
+
+	#try: rmtree(line[:-1])		#for actual files
+	#except: unlink(line[:-1])	#for symlinks
 
 	try: rmtree(line[:-29]  + "xml/" + line[-22:-1])
 	except: unlink(line[:-29]  + "xml/" + line[-22:-1])
--- abid_bot_v2.7/bin/clean_h5folders.sh
+++ abid_bot_v2.7/bin/clean_h5folders.sh
@@ -2,11 +2,18 @@ echo "cleaning h5 and xml folders"
 cur=$PWD
 rm -rf $root/xml/
 cd $root/h5data/
+bad_data=$root/h5data/bad_data
+
+if [ ! -d $bad_data ]; then
+	mkdir $bad_data
+fi
+
 for i in $(ls -d 3d_data*)
 do
 	if [ $(ls $i | wc -l) -eq 0 ]
 	then
-		rm -rf $i
+		mv $i $bad_data/$i
+		#rm -rf $i
 	fi
 done
 cd $cur
