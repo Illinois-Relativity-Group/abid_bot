1b9460c8979c2e197a8ad80e4c3730980104572f
Kyle Nelli <knelli2@illinois.edu>
2020-02-14 10:51:30 -0600

    updated single submission to work with class functionality

--- abid_bot_v2.8/runSingle.sh
+++ abid_bot_v2.8/runSingle.sh
@@ -3,7 +3,7 @@
 #Otherwise use runBundle.sh
 #####begin things you have to change TODO
 
-jobName=bhbh_fields
+jobName=runSingle
 h5dir=$root/h5data
 extrasDir=$root/xml
 h5prefix=3d_data_
@@ -23,9 +23,25 @@ h5dir=$( echo $h5dir | sed "s,/$,,")
 savefolder=$( echo $savefolder | sed "s,/$,,")
 picsavefolder=$( echo $picsavefolder | sed "s,/$,,")
 
+#plotting varibles
+PlotDensAsVol=1 # Plot density in a volume plot
+PlotDensAsIso=0 # Plot density in a pseudocolor plot as isosurfaces
+PlotDensLinear=0 # Plot linear scale density rather than log scale
+PlotVel=0 # Plot velocity arrows
+PlotBsq2r=0 # Plot B squared over 2 rho
+Plotg00=0 # Plot g00 from metric
+refPlot=1 # Reflect plot over xy plane
+cutPlot=0 # only show back half (y>0), needs view like: (0,-x,y)
+
+PlotEvolve=1
+PlotZoom=0
+PlotFlyOver=0
+PlotFlyAround=0
 
 ##########This section submits the rest of the files.
-
+# default is first frame
+foldernum=1
+ranknum=1
 count=1
 DATE=$(date +%y%m%d_%H%M)
 picsavefolder=$picsavedir/"$DATE"_"$jobName"; mkdir -p $picsavefolder
@@ -37,10 +53,12 @@ for dir in $(ls -d ${h5dir}"/"$h5prefix* ); do
     blah=$(ls -d -1 $extrasDir/** | sed -n ${count}p) 
     tosave="$picsavefolder"/"$jobName"_$(printf "%03d" $count)_
 
-	if [ $count -eq 1 ]; then
+	if [ $count -eq $foldernum ]; then
 		for rank in `seq 0 $(( $totranks - 1 ))`; do
-		        echo submitting job $count with rank = $rank
-				qsub -N $jobName"_"$count"_"$rank -v VISITSCRIPT=$visitScript,H5=$dir,EXTRAS=$blah,SAVEFOLDER=$tosave$(printf "%03d" $rank)"_",RANK=$rank,TOTRANKS=$totranks,NUMBFIELDPLOTS=$numBfieldPlots,VECXML=$vecXML,BSQXML=$bsqXML,G00_PSEUDOXML=$g00_pseudoXML,G00_ISOXML=$g00_isoXML,MAXDENS=$maxdensity,RHO_PSEUDOXML=$rho_pseudoXML,RHO_ISOXML=$rho_isoXML $pbsfile
+				if [ $rank -eq $ranknum ]; then
+		        	echo submitting job $count with rank = $rank
+					qsub -N $jobName"_"$count"_"$rank -v VISITSCRIPT=$visitScript,PDAS=$PlotDensAsVol,PDAI=$PlotDensAsIso,PDL=$PlotDensLinear,PV=$PlotVel,PBSQ2R=$PlotBsq2r,PG00=$Plotg00,REFPLOT=$refPlot,CUTPLOT=$cutPlot,PE=$PlotEvolve,PZ=$PlotZoom,PFO=$PlotFlyOver,PFA=$PlotFlyAround,H5=$dir,EXTRAS=$blah,SAVEFOLDER=$tosave$(printf "%03d" $rank)"_",RANK=$rank,TOTRANKS=$totranks,NUMBFIELDPLOTS=$numBfieldPlots,VECXML=$vecXML,BSQXML=$bsqXML,G00_PSEUDOXML=$g00_pseudoXML,G00_ISOXML=$g00_isoXML,MAXDENS=$maxdensity,RHO_PSEUDOXML=$rho_pseudoXML,RHO_ISOXML=$rho_isoXML $pbsfile
+				fi
 		done
 	fi
     count=$((count+1))
