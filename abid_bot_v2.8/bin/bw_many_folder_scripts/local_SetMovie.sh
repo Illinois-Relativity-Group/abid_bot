8040a865775e472b2ebdafbec362e0882fd25729
Kyle Nelli <kcnelli21@gmail.com>
2020-05-27 18:33:36 -0500

    More local scripts

---
+++ abid_bot_v2.8/bin/bw_many_folder_scripts/local_SetMovie.sh
@@ -0,0 +1,7 @@
+xmldir=$root/xml/
+h5prefix=3d_data_
+echo deleting xml files...
+rm -rf ${xmldir}/$h5prefix*/*
+echo creating xml files...
+python setmovie.py $root/ $it $dt $M $offset $fields $numBfieldPlots $g00 $time_offset $vol1XML $vol2XML $view1XML $view2XML $twoColorsSeeds $particleTracer $twoColorsTracer > xml_log.txt
+
