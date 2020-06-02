8059262946f89524ad7e9682bd4d3e5497b6a0e4
Sam Qunell <Sam@qunell.com>
2020-06-02 16:25:40 -0500

    Forgot to add local_cray

--- abid_bot_v2.9/bin/local_setup_frames.sh
+++ abid_bot_v2.9/bin/local_setup_frames.sh
@@ -2,7 +2,7 @@ echo "setting up frames"
 
 cur=$PWD
 cd $root/bin/bw_many_folder_scripts/
-./cray.sh $it $root/h5data
+./local_cray.sh $it $root/h5data
 python rmdupes.py $root/
 . local_SetMovie.sh 
 cd $cur
