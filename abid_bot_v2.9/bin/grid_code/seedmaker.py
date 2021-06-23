5b37399c17f7aa1af03727687e4ea0cd96ae7f03
sigmazhou <zhouj8901@outlook.com>
2021-06-24 04:46:52 +0900

    gridseed code for tilted bh

--- abid_bot_v2.9/bin/grid_code/seedmaker.py
+++ abid_bot_v2.9/bin/grid_code/seedmaker.py
@@ -14,6 +14,28 @@ numBfieldPlots = int(argv[4])
 grid_code_dir = root_dir + "bin/grid_code/"
 bhseed_dir = grid_code_dir + "bhseeds/" 
 
+#thing to change for tilted case
+is_tilted=0 #1 for tilted case in bhdisk
+spinfile=root_dir+"h5data/bhns_BHspin.mon" #set this for tilded case
+#auto generated parts:
+spindata=[]
+xmltimelist=[]
+
+def binsearch(listu,target,begin=0,end=-1,tol=0.01):       #end excluded
+    n=len(listu)
+    if end==-1:
+        end=n
+    if end-begin<=0:
+        #print("target %e not found" % target)
+        return -1
+    mid=int((begin+end)/2)
+    if abs(listu[mid]-target)<tol:
+        return mid
+    elif listu[mid]>target:
+        return binsearch(listu,target,begin,mid)
+    else:
+        return binsearch(listu,target,mid+1,end)
+
 def make_seed_file(coord):
 
 	time = coord[0]
@@ -31,13 +53,38 @@ def make_seed_file(coord):
 			r = 1.0 #0.008 for bhdisk?
 			h = r*2.0
 			m_steps = 20 #10 for bhdisk?
-			for m in range(m_steps):
-				theta = 2*pi*m/m_steps
-				x = xc + r*cos(theta)
-				y = yc + r*sin(theta)
-				z = zc
-				outfile.write(str(x) + "\t" + str(y) + "\t" + str(z+h) + "\n")
-				outfile.write(str(x) + "\t" + str(y) + "\t" + str(z-h) + "\n")
+			
+			if not is_tilted:
+				for m in range(m_steps):
+					theta = 2*pi*m/m_steps
+					x = xc + r*cos(theta)
+					y = yc + r*sin(theta)
+					z = zc
+					outfile.write(str(x) + "\t" + str(y) + "\t" + str(z+h) + "\n")
+					outfile.write(str(x) + "\t" + str(y) + "\t" + str(z-h) + "\n")
+			else:
+				idx=binsearch(xmltimelist,time,tol=0.03)
+				normvec=spindata[idx,1:]
+				normvec /= np.linalg.norm(normvec)
+				cenvec=[xc,yc,zc]
+				pointvec=np.random.randn(3)
+				pointvec -= pointvec.dot(normvec) * normvec
+				pointvec /= np.linalg.norm(pointvec)
+
+				steplen=2*pi*r/m_steps
+
+				for m in range(m_steps+1):
+					p1=cenvec+pointvec*r+normvec*h
+					p2=cenvec+pointvec*r-normvec*h
+					outfile.write(str(p1[0]) + "\t" + str(p1[1]) + "\t" + str(p1[2]) + "\n")
+					outfile.write(str(p2[0]) + "\t" + str(p2[1]) + "\t" + str(p2[2]) + "\n")
+
+					#calc next pointvec
+					shiftvec=np.cross(normvec,pointvec)
+					shiftvec=shiftvec/np.linalg.norm(shiftvec)*steplen
+					pointvec+=shiftvec
+					pointvec /= np.linalg.norm(pointvec)
+
 ############################################################################################################
 print("removing old seeds...")
 if isdir(bhseed_dir):
@@ -47,6 +94,10 @@ makedirs(bhseed_dir)
 
 # set up the timeList and cmList arrays from bhcen.txt
 print("creating new seeds...")
+if is_tilted:
+	spindata=np.loadtxt(spinfile,comments=['#'])
+	xmltimelist=spindata[:,0]
+
 with open(grid_code_dir + "bhcen" + ahtype + ".txt", 'r') as f:
 	coordList = []
 	for line in f:
