909887b0407f701c0963b6e19ead7182e2dadba3
sigmazhou <sigmazhou@outlook.com>
2021-09-15 23:07:55 -0500

    soft link fix

--- abid_bot_v2.10/h5data/dataclean.py
+++ abid_bot_v2.10/h5data/dataclean.py
@@ -1,1 +1,28 @@
-/scratch1/07525/tg868241/bhdisk_45/h5data/dataclean.py
+import numpy as np
+from sys import argv
+import os
+import shutil
+
+dlist=[file for file in os.listdir(".") if file.startswith("3d_data_")]
+
+print(dlist[0])
+
+for d in dlist:
+    data=[file for file in os.listdir(d) if file.startswith("Bx")]
+    #timelist.sort()
+    #print(data)
+    if len(data)<5:
+        print("bad folder "+d)
+        try: shutil.move(d,"bad_data/")
+        except: os.remove(d)
+        continue
+
+    try:
+        f=open(d+"/"+data[0],"r")
+        f.close()
+    except IOError:
+        print(d+"/"+data[0])
+        shutil.move(d,"bad_data/")
+        
+
+
--- abid_bot_v2.10/h5data/h5detect.py
+++ abid_bot_v2.10/h5data/h5detect.py
@@ -1,1 +1,15 @@
-/scratch1/07525/tg868241/bhdisk_45/h5data/h5detect.py
+import h5py
+import os
+import shutil
+root="/home1/07525/tg868241/bhdisk_45/h5data/"
+outfile="h5log.txt"
+rlmax=12
+outy=open(outfile,"w")
+for filename in os.listdir(root):
+	if filename.startswith("3d_data_") and os.path.isdir(root+filename):
+		try:
+			f=h5py.File(root+filename+"/By.file_0.h5")
+		except:
+			shutil.move(root+filename, root+"bad_data/"+filename)
+			outy.write("moved; "+root+filename+"\n")
+outy.close()
